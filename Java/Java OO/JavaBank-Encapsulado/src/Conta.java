c53dbc093b59e3dbceba250fb0910c24c806a68d
Everson Barbosa <eversonlpb@gmail.com>
2021-12-28 19:18:32 -0300

    Getters e Setters

---
+++ Java/Java OO/JavaBank-Encapsulado/src/Conta.java
@@ -0,0 +1,48 @@
+
+public class Conta {
+	
+		 private double saldo;
+		 private int agencia = 42;
+		 private int numeroDaConta;
+		 private Cliente titular;
+		 
+		 public void deposita (double valor) {
+			 this.saldo += valor;
+		 }
+		 public boolean saca (double valor) {
+			 if(this.saldo >= valor) {
+				 this.saldo -= valor; 
+				 System.out.println("Você sacou " + valor);
+				 return true;
+			 } else {
+				 System.out.println("Seu saldo é insuficiente...");
+				 return false;				 
+			 }
+		 }
+		 
+		 public boolean transfere(double valor, Conta destino) {
+			 if (this.saldo >= valor) {
+				 this.saldo -= valor;
+				 destino.deposita(valor);
+				 System.out.println("Transferência realizada com sucesso.");
+				 return true;
+			 } else {
+				 System.out.println("Saldo insuficiente");
+				 return false;
+			 }
+			 
+		 }
+		 
+		 public double getSaldo() {
+			 return this.saldo;
+		 }
+		 
+		 public int getNumeroDaConta() {
+			 return this.numeroDaConta;
+		 }
+		 
+		 public void setNumeroDaConta (int novoNumeroDaConta) {
+			 this.numeroDaConta = novoNumeroDaConta;
+		 }
+		 
+}
---
+++ Java/Java OO/JavaBank-Encapsulado/src/TestaGeteSet.java
@@ -0,0 +1,8 @@
+
+public class TestaGeteSet {
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.setNumeroDaConta(1337);
+		System.out.println(conta.getNumeroDaConta());
+	}
+}
